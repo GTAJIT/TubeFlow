f659f6d977cefe8e5011ea6bf784e22e1fe0a1b8
MohakGupta2004 <mohakgupta500@gmail.com>
2024-11-19 21:05:09 +0530

    Successfully deployed to vercel

--- Backend/src/index.ts
+++
@@ -1,9 +0,0 @@
-import app from './app';
-import { VercelRequest, VercelResponse } from '@vercel/node';
-import { createServer } from 'http';
-
-const server = createServer((req, res) => app(req as any, res as any));
-
-export default (req: VercelRequest, res: VercelResponse) => {
-  server.emit('request', req, res);
-};
