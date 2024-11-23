ea71dc3cc28e2573ef28f0f9dfdc281573ff3d92
MohakGupta2004 <mohakgupta500@gmail.com>
2024-11-24 03:41:52 +0530

    Cloudinary function add (not working properly)

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,23 @@
 import { Hono } from 'hono'
 import appRouter from './routes/index.routes'
 const app = new Hono()
-
+import { v2 as cloudinary } from "cloudinary";
+import { config } from 'dotenv';
+import { Context } from 'hono';
+config({path: '../.env'})
+app.use(async(c:Context, next)=>{
+    cloudinary.config({
+        cloud_name: c.env.CLOUDINARY_CLOUD_NAME,
+        api_key: c.env.CLOUDINARY_API_KEY,
+        api_secret: c.env.CLOUDINARY_API_SECRET
+    });
+    console.log('Cloudinary Config:', {
+      cloud_name: c.env.CLOUDINARY_CLOUD_NAME,
+      api_key: c.env.CLOUDINARY_API_KEY ? 'Loaded' : 'Missing',
+      api_secret: c.env.CLOUDINARY_API_SECRET ? 'Loaded' : 'Missing',
+    });
+    await next();
+})
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
